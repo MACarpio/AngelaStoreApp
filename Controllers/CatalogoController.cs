6c2dfa9142503e5d0443c77b37e93898c447b851
Mauricio Apaza <mpacarpio@gmail.com>
2021-10-29 16:15:47 -0500

    Agregar buscar producto

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -28,6 +28,17 @@ namespace AngelaStoreApp.Controllers
             return View(await productos.ToListAsync());
         }
 
+         [HttpGet]
+        public async Task<IActionResult> Index(String Empsearch){
+            ViewData["Getemployeedetails"]=Empsearch;
+            var empquery=from x in _context.DataProduct select x;
+            if(!string.IsNullOrEmpty(Empsearch)){
+                empquery=empquery.Where(x =>x.Name.Contains(Empsearch))  ;
+            }
+            return View(await empquery.AsNoTracking().ToListAsync());
+
+        }
+
         public async Task<IActionResult> Details(int? id)
         {
             Product objProduct = await _context.DataProduct.FindAsync(id);
